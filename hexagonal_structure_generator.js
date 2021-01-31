c42ee06f66d6d391f3c116918576549ccece92cc
mukitul <mukitulislamratul@gmail.com>
2021-01-31 23:35:21 +0600

    done

--- hexagonal_structure_generator.js
+++ hexagonal_structure_generator.js
@@ -3,8 +3,11 @@ var fs = require('fs');
 let rawdata = fs.readFileSync('folder_str.json');
 let hierarchy = JSON.parse(rawdata);
 
-hexgon(hierarchy.name, hierarchy.layer, hierarchy.root);
+if (!fs.existsSync(hierarchy.root)) {
+    fs.mkdirSync(hierarchy.root);
+}
 
+hexgon(hierarchy.name, hierarchy.layer, hierarchy.root);
 
 function hexgon(folderName, hierarchy, str) {
 
@@ -14,7 +17,9 @@ function hexgon(folderName, hierarchy, str) {
     }
 
     for (let index = 0; index < hierarchy.length; index++) {
-        dfs(hierarchy[index].name, hierarchy[index].layer, str);
+        hexgon(hierarchy[index].name, hierarchy[index].layer, str);
     }
 
 }
+
+console.log("hexagonal folder structure created: ", hierarchy.root)
