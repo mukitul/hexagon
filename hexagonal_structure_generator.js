509d4d8eadf7359f1e4fc46989f4a18e573d18e5
mukitul <mukitulislamratul@gmail.com>
2021-03-26 12:29:25 +0600

    user input

--- hexagonal_structure_generator.js
+++ hexagonal_structure_generator.js
@@ -1,25 +1,50 @@
-var fs = require('fs');
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-let rawdata = fs.readFileSync('folder_str.json');
-let hierarchy = JSON.parse(rawdata);
+const fs = require('fs');
+const rawdata = fs.readFileSync('folder_str.json');
 
-if (!fs.existsSync(hierarchy.root)) {
-    fs.mkdirSync(hierarchy.root);
-}
+let pkgName = '';
+let directory = '';
+rl.question("Where do you want to create folder ? Answer:", function (dir) {
+    rl.question("What is your Root  Package Name ? Answer:", function (rName) {
+        rl.question("What is your  Feature Package Name ? Answer:", function (name) {
+            rPkgName = `${rName}`;
+            pkgName = `${name}`;
+            directory = `${dir}`;
 
-hexgon(hierarchy.name, hierarchy.layer, hierarchy.root);
+            let hierarchy = JSON.parse(rawdata);
 
-function hexgon(folderName, hierarchy, str) {
+            hierarchy['name'] = rPkgName;
+            hierarchy['root'] = directory;
+            hierarchy.layer[0]['name'] = pkgName;
 
-    str = str + "/" + folderName;
-    if (!fs.existsSync(str)) {
-        fs.mkdirSync(str);
-    }
 
-    for (let index = 0; index < hierarchy.length; index++) {
-        hexgon(hierarchy[index].name, hierarchy[index].layer, str);
-    }
+            if (!fs.existsSync(hierarchy.root)) {
+                fs.mkdirSync(hierarchy.root);
+            }
 
-}
+            hexgon(hierarchy.name, hierarchy.layer, hierarchy.root);
 
-console.log("hexagonal folder structure created: ", hierarchy.root)
+            function hexgon(folderName, hierarchy, str) {
+
+                str = str + "/" + folderName;
+                if (!fs.existsSync(str)) {
+                    fs.mkdirSync(str);
+                }
+
+                for (let index = 0; index < hierarchy.length; index++) {
+                    hexgon(hierarchy[index].name, hierarchy[index].layer, str);
+                }
+
+            }
+
+            console.log("hexagonal folder structure created: ", hierarchy.root)
+
+            rl.close();
+        });
+    });
+});
